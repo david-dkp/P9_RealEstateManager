ed10586d0baf52a93e7756170cd0c2968df0a48e
David DEKEUWER <david.dekeuwer@gmail.com>
2021-05-20 14:22:15 +0200

    Mirgrate to Kotlin, add all dependencies required

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.openclassrooms.realestatemanager
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
