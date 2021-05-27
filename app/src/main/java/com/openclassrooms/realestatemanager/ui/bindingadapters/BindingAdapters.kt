fc1f1dc35d1030df3dba60f3fd80343d6b16b697
David DEKEUWER <david.dekeuwer@gmail.com>
2021-05-27 18:31:16 +0200

    Fix binding adapters not being static

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/bindingadapters/BindingAdapters.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/bindingadapters/BindingAdapters.kt
@@ -4,25 +4,29 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
-object BindingAdapters {
+@BindingAdapter("app:imagePath", "app:circleCrop", "app:centerCrop", requireAll = false)
+fun loadImageFromStoragePath(
+    imageView: ImageView,
+    imagePath: String?,
+    circleCrop: Boolean? = false,
+    centerCrop: Boolean? = true
+) {
+    imagePath?.let {
+        val requestBuilder = Glide.with(imageView)
+            .load(Firebase.storage.getReference(imagePath))
 
-    @BindingAdapter("app:imagePath", "app:circleCrop", "app:centerCrop", requireAll = false)
-    fun loadImageFromStoragePath(imageView: ImageView, imagePath: String?, circleCrop: Boolean? = false, centerCrop: Boolean? = true) {
-        imagePath?.let {
-            val requestBuilder = Glide.with(imageView)
-                .load(it)
-
-            if (centerCrop == true) {
-                requestBuilder.centerCrop()
-            }
-
-            if (circleCrop == true) {
-                requestBuilder.transform(CircleCrop())
-            }
-
-            requestBuilder.into(imageView)
+        if (centerCrop == true) {
+            requestBuilder.centerCrop()
         }
-    }
 
+        if (circleCrop == true) {
+            requestBuilder.transform(CircleCrop())
+        }
+
+        requestBuilder.into(imageView)
+    }
 }
+
