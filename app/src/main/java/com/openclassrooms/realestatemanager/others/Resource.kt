64b25d68f25938df05ce118e7ee7c3c915a3094e
David DEKEUWER <david.dekeuwer@gmail.com>
2021-05-27 17:03:32 +0200

    Fix Resource wrong accesibility to fields

--- app/src/main/java/com/openclassrooms/realestatemanager/others/Resource.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/others/Resource.kt
@@ -1,6 +1,6 @@
 package com.openclassrooms.realestatemanager.others
 
-sealed class Resource<T>(data: T? = null, errorType: ErrorType? = null) {
+sealed class Resource<T>(val data: T? = null, val errorType: ErrorType? = null) {
     class Success<T>(data: T? = null) : Resource<T>(data)
     class Loading<T>(data: T? = null) : Resource<T>(data)
     class Error<T>(data: T? = null, errorType: ErrorType? = null) : Resource<T>(data, errorType)
