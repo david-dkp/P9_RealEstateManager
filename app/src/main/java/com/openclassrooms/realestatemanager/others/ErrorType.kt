0d18aafc7804435ca20583f0f849c072fb26bd94
David DEKEUWER <david.dekeuwer@gmail.com>
2021-05-27 18:35:29 +0200

    Bug fix

--- app/src/main/java/com/openclassrooms/realestatemanager/others/ErrorType.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/others/ErrorType.kt
@@ -1,8 +1,8 @@
 package com.openclassrooms.realestatemanager.others
 
-sealed class ErrorType(message: String? = null) {
+sealed class ErrorType(val message: String? = null) {
     object NoInternet : ErrorType()
     object CantFoundAddress: ErrorType()
     object WrongCredential : ErrorType()
-    class Unknown(message: String? = null) : ErrorType(message)
+    class Unknown(message: String?) : ErrorType(message)
 }
